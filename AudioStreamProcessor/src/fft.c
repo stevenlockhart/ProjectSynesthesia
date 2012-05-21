4bb96e093950cfd944d86340813884969e7a009d
root <root@beagleboard.(none)>
2012-05-21 11:07:45 +0100

    LoMdHi Spectrum computation works

--- AudioStreamProcessor/src/fft.c
+++ AudioStreamProcessor/src/fft.c
@@ -29,6 +29,7 @@ int calculate_spectrum(unsigned int n_frames, fb_t frame_buffer,
   double freq;
   double amp;
   unsigned int n;
+  spec->lo = 0; spec->md = 0; spec->hi = 0;
   for (n = 0; n < frame_buffer->num_elements; n++) {
     freq = (double)frame_buffer->num_elements / (n + 1);
     amp = (double)*(*(fftw_complex *)out[n]); // Yikes
@@ -38,19 +39,18 @@ int calculate_spectrum(unsigned int n_frames, fb_t frame_buffer,
     // Build spectrum ranges
     if (amp > 0) {
       if (freq < 1000) {
-        spec->lo = amp;
+        spec->lo += amp;
       } else if (1000 <= freq && freq < 10000) {
-        spec->md = amp;
+        spec->md += amp;
       } else {
-        spec->hi = amp;
+        spec->hi += amp;
       }
     }
   }
 
   // TEMP
   printf("Spectrum: {%8f, %8f, %8f}\n",
-  //         amp, amp, amp);
-                     spec->lo, spec->md, spec->hi);
+         spec->lo, spec->md, spec->hi);
 
   // Update spectrum
   // TODO
