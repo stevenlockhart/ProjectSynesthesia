0c265d028565b003b3de80e59fe14b44a1b4c378
Ian Finder <ian.finder@gmail.com>
2012-05-28 01:37:23 -0700

    Cleanup

--- LEDArrayController/lib/cop-lights.c
+++ LEDArrayController/lib/cop-lights.c
@@ -1,88 +1,32 @@
 #include "led.h"
 #include <stdio.h>
-#define LEDS 120    // Max LEDS
+#define LEDS 120    // Number of LEDs connected
 
-// DOES RED / GREEN COP LIGHTS ACROSS THE STRAND
+/*
+    Demo code for led.h - (c) 2012 Ian Finder
+    DOES RED / BLUE COP LIGHTS ACROSS THE STRAND
+ 
+*/
 
-
-// DEMO CODE
 int main (int argc, char **argv) {
 	static char device[] = "/dev/tty.usbserial-FTELSJ0N";
-	STRAND * led = build_strand(LEDS, device, 460800);    // Number of LEDs, Device, Baud
+	STRAND * led = build_strand(LEDS, device, 460800);                                          // Number of LEDs, Device, Baud
 	strand_info(led);
 	int i = 0;
     int count = 0;
-	while (count < 200) {   // Run length
+	while (count < 200) {                                                                       // Run length
 		int z = i;
 		for (i = 0 ; i < led->leds ; i++) {
             if (i < (LEDS/2)) {
-                led->colors[i*3] = ((count % 2)==1)? 0:255;                                     // ARE
-                led->colors[(i*3) + 1] = ((count % 2)==1)? 0:0;                               // GEE
-                led->colors[(i*3) + 2] = ((count % 2)==1)? 255:0;       
+                led->colors[i*3] = ((count % 2)==1)? 0:255;                                     // RED
+                led->colors[(i*3) + 1] = ((count % 2)==1)? 0:0;                                 // GREEN
+                led->colors[(i*3) + 2] = ((count % 2)==1)? 255:0;                               // BLUE
             } else {
-                led->colors[i*3] = ((count % 2)==0)? 255:0;                                     // ARE
-                led->colors[(i*3) + 1] = ((count % 2)==0)? 0:0;                               // GEE
-                led->colors[(i*3) + 2] = ((count % 2)==0)? 0:255;                               // BEE
+                led->colors[i*3] = ((count % 2)==0)? 255:0;                                     // RED
+                led->colors[(i*3) + 1] = ((count % 2)==0)? 0:0;                                 // GREEN
+                led->colors[(i*3) + 2] = ((count % 2)==0)? 0:255;                               // BLUE
             }
-            /*
-             switch (i % 10) {
-             case 0:
-             led->colors[i*3] = 0;  //led->colors[(i*3) + 1] - 5;              // RED
-             led->colors[(i*3) + 1] = 0;// led->colors[(i*3) + 2] - 10;    // GREEN
-             led->colors[(i*3) + 2] = 0;//led->colors[(i*3)] + 20;    // BLEW
-             break;
-             case 1:
-             led->colors[i*3] = 0;                                     // ARE
-             led->colors[(i*3) + 1] = 0;                               // GEE
-             led->colors[(i*3) + 2] = 0;                               // BEE
-             break;
-             case 2:
-             led->colors[i*3] = 0;                                     // ARE
-             led->colors[(i*3) + 1] = 0;                               // GEE
-             led->colors[(i*3) + 2] = 0;                               // BEE
-             break;
-             case 3:
-             led->colors[i*3] = 0;                                     // ARE
-             led->colors[(i*3) + 1] = 0;                               // GEE
-             led->colors[(i*3) + 2] = 0;                               // BEE
-             break;
-             case 4:
-             led->colors[i*3] = 0;                                     // ARE
-             led->colors[(i*3) + 1] = 0;                               // GEE
-             led->colors[(i*3) + 2] = 0;                               // BEE
-             break;
-             case 5:
-             led->colors[i*3] = 0;                                     // ARE
-             led->colors[(i*3) + 1] = 0;                               // GEE
-             led->colors[(i*3) + 2] = 0;                               // BEE
-             break;
-             case 6:
-             led->colors[i*3] = ((count % 2)==1)? 0:0;                                     // ARE
-             led->colors[(i*3) + 1] = ((count % 2)==1)? 0:0;                               // GEE
-             led->colors[(i*3) + 2] = ((count % 2)==1)? 255:0;                               // BEE
-             break;
-             case 7:
-             led->colors[i*3] = 0;                                     // ARE
-             led->colors[(i*3) + 1] = 0;                               // GEE
-             led->colors[(i*3) + 2] = 0;                               // BEE
-             break;
-             case 8:
-             led->colors[i*3] = 0;                                     // ARE
-             led->colors[(i*3) + 1] = 0;                               // GEE
-             led->colors[(i*3) + 2] = 0;                               // BEE
-             break;
-             case 9:
-             led->colors[i*3] = ((count % 2)==0)? 255:0;                                     // ARE
-             led->colors[(i*3) + 1] = ((count % 2)==0)? 0:0;                               // GEE
-             led->colors[(i*3) + 2] = ((count % 2)==0)? 0:0;                               // BEE
-             break;
-             default:
-             break;
-             
-             }
-             */
         }
-        //printf("LED %3d : %3d %3d %3d\n",i,led->colors[i*3],led->colors[(i*3) + 1], led->colors[(i*3) + 2]);
 		update_strand(led);
         usleep(50000);
         count++;
