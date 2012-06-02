099726da729559bd66e2ff725edf8ba3c81847a5
root <root@id-engager.console>
2012-06-01 21:41:00 -0700

    WTF

--- LEDArrayController/lib/main.c
+++ LEDArrayController/lib/main.c
@@ -1,7 +1,7 @@
 #define LEDS 30   // Number of LEDs connected
 #define RUNS 40
-#define BAUD 921600
-#define DEVICE "/dev/tty.usbserial-FTELSJ0N"
+#define BAUD 115200
+#define DEVICE "/dev/ttyUSB0"
 
 // Point me at your test pattern!
 #include "cop-lights.c"
